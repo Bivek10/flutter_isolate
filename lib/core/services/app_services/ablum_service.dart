fe9147b7170553380cc8d0adeff4370ff1928e92
Bivek10 <bibekkarki662@gmail.com>
2023-11-07 08:37:03 +0545

    feat: imp run Isolate repo

--- lib/core/services/app_services/ablum_service.dart
+++ lib/core/services/app_services/ablum_service.dart
@@ -1,26 +1,48 @@
 import 'dart:convert';
+import 'dart:isolate';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_isolate/core/services/app_repository/album_repo.dart';
+import 'package:flutter_isolate/core/utils/isolate.dart';
 import 'package:flutter_isolate/model/album_reponse_model.dart';
 
 class LocalAlbum implements AlbumRepo {
   @override
   Future<List<AlbumResponse>> getAlbum() async {
     String albumData = await rootBundle.loadString('assets/tempdata.json');
-
-    return await compute(parseAlbum, albumData);
-    // return await compute(AlbumResponse.fromJson, albumJson);
-    // IsolateManager isolate = IsolateManager();
-    // IsolateRepository isolateInstance =
-    //     isolate.getIsolate(isolateType: "inbuilt");
-
-    //return await isolateInstance.initIsolate(AlbumResponse.fromJson, albumJson);
+    final List<dynamic> albumJson = jsonDecode(albumData);
+    return albumJson.map((e) => AlbumResponse.fromJson(e)).toList();
   }
 
-  List<AlbumResponse> parseAlbum(String ablumData) {
-    final List<dynamic> ablumJson = jsonDecode(ablumData);
-    return ablumJson.map((e) => AlbumResponse.fromJson(e)).toList();
+  List<AlbumResponse> parseAlbum(String data) {
+    final List<dynamic> jsonData = jsonDecode(data);
+    return jsonData.map((e) => AlbumResponse.fromJson(e)).toList();
+  }
+
+  @override
+  Future<List<AlbumResponse>> runIsolate() async {
+    String albumData = await rootBundle.loadString('assets/tempdata.json');
+    return await compute(parseAlbum, albumData);
+  }
+}
+
+class LocalAlbumService implements AlbumRepo {
+  @override
+  Future<List<AlbumResponse>> getAlbum() async {
+    String albumData = await rootBundle.loadString('assets/tempdata.json');
+    final List<dynamic> albumJson = jsonDecode(albumData);
+    return albumJson.map((e) => AlbumResponse.fromJson(e)).toList();
+  }
+
+  @override
+  Future<List<AlbumResponse>> runIsolate() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    return await Isolate.run(getAlbum);
+    // IsolateManager isolateManager = IsolateManager();
+    // IsolateRepository isolate =
+    //     isolateManager.getIsolate(isolateType: "custom");
+    // return await isolate.initIsolate(getAlbum);
   }
 }
